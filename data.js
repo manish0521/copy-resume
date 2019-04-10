5efeaa707d959ebfdfe7b86e01ac935e96064cde
manish singh <manish.singh@codeimmersives.com>
2019-04-10 15:21:07 -0400

    .json added

---
+++ data.js
@@ -0,0 +1,57 @@
+{
+ "manish" : {
+        "bio" : [
+            "Multi-lingual Technical Professional (Nepalese, Hindi, Urdu) | Retired U.S. Army Veteran leveraging a background of driving strategy, direction, and oversight for end-to-end organizational development ranging across public sector and private sector environments, with a particular focus on ensuring quality standards, compliance, policy and procedure adherence. Maintained and oversaw program management across various process, policy, and operational improvements, specializing in implementing and improving global process improvements within the organization. Possess extensive knowledge in composite risk management, training & development, and process improvement. Career supported by specialized military leadership trainings, certifications, Bachelor of Science in Chemistry/Mathematics, and Masters in Public Administration with a concentration in Public Policy."
+        
+        ],
+
+        "experience": [
+            "U.S. Army Corp of Engineers : Economist (2017-2018)",
+            "Internal Revenue Service (IRS) : Paralegal Specialist: (Internship: 2017)",
+            "Baruch College, CUNY : Vice-President: Graduate Student Assembly (2017-2018)",
+            "U.S. Army : Orthopedic Specialist (2014-2017)"
+        ],
+
+        "skills": [
+            "Cross Functional Leadership",
+            "Qualitative Assurance",
+            "Data Analysis",
+            "Inferential statistics",
+            "Strategic Planning | Forecasting",
+            "Emergency Manageme",
+            "Career | Performance Evaluation",
+            "Composite Risk Management",
+            "Instructor",
+            "Descriptive Statistics"
+        ],
+
+        "education": [
+            "Web Development | Code Immersisves, New York (Dec 2019) ",
+            "Masters of Public Administration | Baruch College at CUNY (Dec 2018)",
+            "Bachelor of Science in Chemistry | McNeese State University (Dec 2013)",
+            "~ Concentration: Biochemistry | Mathematics",
+            "Orthopedic Technician Specialty Course (Dec 2014)",
+            "HAZMAT Certified | OSHA Certified"
+        ]
+    }
+
+"intro" : {
+    "name" : "Name: Manish Sing"
+    "email" : "Email: manishsingh21@hotmail.com"
+    "phone" : "Phone: 3375290150"
+
+    }
+
+"quotes" : { [
+    "quote 1"
+    ],
+    [
+       "qiote 2" 
+    ]
+    
+}
+
+
+}
+    
+   
---
+++ main.js
@@ -0,0 +1,102 @@
+window.onload = init;
+
+function init() {
+    
+    displayAll();
+}
+
+
+
+function displayAll() {
+
+    // for(let i = 0; i < manish.bio.length; i++) {
+        display(manish);
+        quotesDisplay();
+        myIntro () ;
+    // }
+
+    
+}
+
+function display(profile) {
+    const bioUl = document.querySelector('#bio');
+    const skillsUl = document.querySelector('#skill');
+    const experienceUl = document.querySelector('#experience');
+    const educationUl = document.querySelector('#education');
+    
+
+    
+    // const profileLi = document.createElement('li');
+    // const profileData = document.createElement('ul');
+    // profileLi.appendChild(profileData);
+    // bioUl.appendChild(profileLi);
+
+    
+
+  
+    for(let i = 0; i < manish.bio.length; i++){
+
+        const textLi = document.createElement('li');
+        // const bioData = document.createElement('ul');
+        textLi.innerText = profile.bio ;
+        // textLi.appendChild(bioData);
+        bioUl.appendChild(textLi);
+    }
+        
+    for(let i = 0; i < manish.skills.length; i++){
+
+        
+        const skillsLi = document.createElement('li');
+        // const skillsData = document.createElement('ul');
+        skillsLi.innerText = profile.skills[i] ;
+        // skillsLi.appendChild(skillsData);
+        skillsUl.appendChild(skillsLi);
+        
+    }
+
+    for(let i = 0; i < manish.experience.length; i++){
+
+        const experienceLi = document.createElement('li');
+        // const bioData = document.createElement('ul');
+        experienceLi.innerText = profile.experience[i] ;
+        // textLi.appendChild(bioData);
+        experienceUl.appendChild(experienceLi);
+    }
+        
+    for(let i = 0; i < manish.education.length; i++){
+
+        
+        const educationLi = document.createElement('li');
+        // const skillsData = document.createElement('ul');
+        educationLi.innerText = profile.education[i] ;
+        // skillsLi.appendChild(skillsData);
+        educationUl.appendChild(educationLi);
+        
+    }
+    
+}
+
+
+function quotesDisplay () {
+    let randomNum = Math.floor(Math.random()) * quotes.length;
+    document.getElementById('quote').innerHTML = quotes[randomNum];
+
+
+}
+
+function myIntro () {
+    const introSelector = document.querySelector('#intro');
+
+       
+     // const bioData = document.createElement('ul');
+        // // style.listStyle = 'none'
+        Object.values(intro).forEach(
+            (data) => {
+                const introLi = document.createElement('p')
+                introLi.innerText = data ;
+                introSelector.appendChild(introLi);
+            }
+        )
+       
+}
+
